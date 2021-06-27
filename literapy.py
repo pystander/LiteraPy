b07a7894a74e4ce9f6d3f32fdd5c98c3c23fec68
pystander <67277708+pystander@users.noreply.github.com>
2021-06-27 17:13:08 +0800

    Update literapy.py

--- literapy.py
+++ literapy.py
@@ -47,20 +47,19 @@ def search():
         
 def pinyin():
     word = input("Search for pinyin:\n")
-    idx = [ce_line.index(i) for i in ce_line if i.startswith(word + " ")]
-    pylist = []
+    matched = []
     t_start = time.time()
     
-    count = len(idx)
+    for i in ce_line:
+        if word + ' ' == i[0:2] or ' ' + word + ' ' == i[1:4]:
+            start = i.find('[')
+            end = i.find(']')
+            matched.append(i[start+1:end])
+            
+    count = len(matched)
     if count > 0:
-        for i in range(count):
-            start = ce_line[idx[i]].find('[')
-            end = ce_line[idx[i]].find(']')
-            pylist.append(ce_line[idx[i]][start+1:end])
-
         interval = '{0:.3f}'.format(time.time() - t_start)
         print("Total of " + str(count) + " record(s) (" + str(interval) + " seconds)")
-        return pylist
+        return matched
     else:
         print("No matched record")
-    
