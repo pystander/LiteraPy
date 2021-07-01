5f118a8ed7780cc33e833a77c3c8675cac7dd2b1
pystander <67277708+pystander@users.noreply.github.com>
2021-07-01 17:54:54 +0800

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -5,15 +5,13 @@ import re
 import time
 import jieba # Package 'jieba' required
 
-jieba.enable_paddle()
-
 def analyse():
     txt = input("Enter the whole paragraph / sentence(s): \n")
-    clauses = re.split('。|，|；|：|？|！',txt)
+    clauses = list(filter(None,re.split('。|，|；|：|？|！|「|」|“ |”',txt)))
     t_start = time.time()
     
     for clause in clauses:
-        seg_list = jieba.lcut(clause,use_paddle=True)
+        seg_list = jieba.lcut(clause)
         print('/'.join(seg_list))
         
     interval = '{0:.3f}'.format(time.time() - t_start)
