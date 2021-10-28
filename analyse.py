e81c0db1305c07bb6d51bffbd8aec8edf5819fbf
pystander <67277708+pystander@users.noreply.github.com>
2021-10-28 20:29:03 +0800

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -15,12 +15,40 @@ from collections import Counter
 jieba.enable_paddle()
 jieba.set_dictionary('dict/dict.txt')
 
-# Default settings
-class Setting:
-    fq_mode = True
-
 # Define functions
-def analyse():
+def analyse(fq_mode=True):
+    txt = input("Enter the whole paragraph / sentence(s): \n")
+    
+    if txt == "":
+        return None
+    
+    clauses = list(filter(None,re.split('。|，|；|：|、|？|！|「|」|“ |”|（|）',txt)))
+    temp = []
+    result = []
+    t_start = time.time()
+    
+    # Cut by jieba
+    for clause in clauses:
+        seg_list = jieba.lcut(clause, use_paddle=True, cut_all=True)
+        temp.append(' '.join(seg_list))
+        
+    result = checklist(list(itertools.chain(*result)))
+    result_fq = dict(Counter(result))
+    
+    # Word frequency count
+    if fq_mode:
+        with open('dict/frequency.txt',encoding='utf-8') as f:
+            fq = ast.literal_eval(f.read())
+            learnt = dict(Counter(fq) + Counter(result_fq))
+            
+            with open('dict/frequency.txt','r+',encoding='utf-8') as f:
+                f.write(str(learnt))
+                
+    interval = '{0:.3f}'.format(time.time() - t_start)
+    print("Time interval: " + str(interval) + " seconds")
+    return result
+
+def analyse_jieba(fq_mode=True):
     txt = input("Enter the whole paragraph / sentence(s): \n")
     
     if txt == "":
@@ -40,11 +68,11 @@ def analyse():
     for i in temp:
         result.append(i.split(' '))
         
-    result = checklist(list(itertools.chain(*result)))
+    result = list(itertools.chain(*result))
     result_fq = dict(Counter(result))
     
     # Word frequency count
-    if Setting.fq_mode:
+    if fq_mode:
         with open('dict/frequency.txt',encoding='utf-8') as f:
             fq = ast.literal_eval(f.read())
             learnt = dict(Counter(fq) + Counter(result_fq))
@@ -78,18 +106,3 @@ def initfq():
         f.write('{}')
         f.truncate()
         print("Frequency file cleared")
-
-def fqmode():
-    print("Current FQ mode: " + str(Setting.fq_mode))
-    code = input("Enter preferred mode: ['True', 'False']\n")
-    
-    if code == "True":
-        Setting.fq_mode = True
-        print("FQ mode changed to: " + str(Setting.fq_mode))
-        
-    elif code == "False":
-        Setting.fq_mode = False
-        print("FQ mode changed to: " + str(Setting.fq_mode))
-        
-    else:
-        print("FQ mode not supported")
