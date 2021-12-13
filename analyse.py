00ddd0c21fa1049120f6263f68247185e6330bd7
pystander <67277708+pystander@users.noreply.github.com>
2021-12-13 21:46:13 +0800

    Update analyse.py

--- analyse.py
+++ analyse.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# LiteraPy v1.0.9
+# LiteraPy v1.0.0
 # Copyright (c) 2021 pystander
 
 # Import libraries
@@ -16,7 +16,7 @@ jieba.enable_paddle()
 jieba.set_dictionary('dict/dict.txt')
 
 # Define functions
-def analyse(fq_mode=True):
+def analyse(fq_mode=False, check_dict=False):
     txt = input("Enter the whole paragraph / sentence(s): \n")
     
     if txt == "":
@@ -36,43 +36,11 @@ def analyse(fq_mode=True):
     for i in temp:
         result.append(i.split(' '))
         
-    result = checklist(list(itertools.chain(*result)))
-    result_fq = dict(Counter(result))
-    
-    # Word frequency count
-    if fq_mode:
-        with open('dict/frequency.txt',encoding='utf-8') as f:
-            fq = ast.literal_eval(f.read())
-            learnt = dict(Counter(fq) + Counter(result_fq))
-            
-            with open('dict/frequency.txt','r+',encoding='utf-8') as f:
-                f.write(str(learnt))
-                
-    interval = '{0:.3f}'.format(time.time() - t_start)
-    print("Time interval: " + str(interval) + " seconds")
-    return result
-
-def analyse_jieba(fq_mode=True):
-    txt = input("Enter the whole paragraph / sentence(s): \n")
-    
-    if txt == "":
-        return None
-    
-    clauses = list(filter(None,re.split('。|，|；|：|、|？|！|「|」|“ |”|（|）',txt)))
-    temp = []
-    result = []
-    t_start = time.time()
-    
-    # Cut by jieba
-    for clause in clauses:
-        seg_list = jieba.lcut(clause, use_paddle=True, cut_all=True)
-        temp.append(' '.join(seg_list))
+    if check_dict:
+        result = checklist(list(itertools.chain(*result)))
+    else:
+        result = list(itertools.chain(*result))
         
-    # Split into phrases
-    for i in temp:
-        result.append(i.split(' '))
-        
-    result = list(itertools.chain(*result))
     result_fq = dict(Counter(result))
     
     # Word frequency count
