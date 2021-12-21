79a1c9415299006a4d50a293ecc9241dfeb6dd1d
pystander <67277708+pystander@users.noreply.github.com>
2021-12-21 17:33:13 +0800

    Update dict.py

--- dict.py
+++ dict.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2021 pystander
 
 # Import libraries
+import time
 from litera import *
 
 dict_path = 'dict/dict.txt'
@@ -21,11 +22,15 @@ def dmod(mode = 'r', data = ''):
     elif mode == 'w' or mode == 'a':
         with open(dict_path, mode, encoding='utf-8') as f:
             f.write(data + '\n')
-            print("Dictionary appended")
+            print("Dictionary updated")
 
 def parser(dict_list: list, delimiter: str, new_delimiter: str):
+    t_start = time.time()
+    
     with open(dict_path, 'a', encoding='utf-8') as f:
         for i in range(len(dict_list)):
-            f.write(dict_list[i].replace(delimiter, new_delimiter) + '\n')
-    
-    print("Dictionary file updated")
+            add_list = dict_list[i].replace(delimiter, new_delimiter)
+            f.write(add_list + '\n')
+            
+    interval = '{0:.3f}'.format(time.time() - t_start)
+    print("Dictionary file updated (" + str(interval) + " seconds)")
