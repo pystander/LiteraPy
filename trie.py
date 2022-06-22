2a64f9e9f13e4360f8cd1b3a09fbac518f3c0744
pystander <67277708+pystander@users.noreply.github.com>
2022-06-22 12:00:32 +0800

    Update trie.py

--- trie.py
+++ trie.py
@@ -4,35 +4,42 @@
 
 # Import libraries
 import collections
+from distutils.command.build import build
+import time
 
 DICT_PATH = 'dict/dict.txt'
 
-# Load dictionary
+# Load dictionary and trie
 with open(DICT_PATH, 'r', encoding='utf-8') as f:
     cidian = [line.rstrip('\n') for line in f]
     print("Cidian loaded")
 
 # Node
 class TrieNode:
-    def __init__(self):
+    def __init__(self, char: str):
         self.child = collections.defaultdict(TrieNode)
-        self.is_word = False
+        self.char = char
+        self.is_end = False
 
 # Trie
 class Trie:
     def __init__(self):
-        self.root = TrieNode()
+        self.root = TrieNode("")
 
     def insert(self, word: str):
         node = self.root
 
         for char in word:
-            if char not in node.child:
-                node.child[char] = TrieNode()
+            if char in node.child:
+                node = node.child[char]
+            else:
+                new_node = TrieNode(char)
+                node.child[char] = new_node
+                node = new_node
 
-        node.is_word = True
+        node.is_end = True
 
-    def search(self, word):
+    def search(self, word: str):
         node = self.root
 
         for char in word:
@@ -41,15 +48,48 @@ class Trie:
             if not node:
                 return False
 
-        return node.is_word
+        return node.is_end
 
-    def start_wtih(self, prefix):
+    def start_wtih(self, prefix: str):
         node = self.root
 
         for char in prefix:
-            node = node.child.get(char)
+            node = node.child[char]
 
             if not node:
                 return False
 
         return True
+
+    def prefix_search(self, prefix: str):
+        node = self.root
+
+        for char in prefix:
+            if char in node.child:
+                node = node.child[char]
+            else:
+                return []
+
+        self.result = []
+        self.dfs(node, prefix[:-1])
+
+        return self.result
+
+    def dfs(self, node: TrieNode, prefix: str):
+        if node.is_end:
+            self.result.append((prefix + node.char))
+
+        for child in node.child.values():
+            self.dfs(child, prefix + node.char)
+
+    # Build trie by dict
+    def build_trie(self, dict_list: list=cidian, delimiter: str='\t'):
+        t_start = time.time()
+
+        for line in dict_list:
+            chunks = line.split(delimiter)
+            self.insert(chunks[0])
+            self.insert(chunks[1])
+
+        interval = '{0:.3f}'.format(time.time() - t_start)
+        print("Trie updated (" + str(interval) + " seconds)")
